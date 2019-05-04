9d02c798bb5f0e4ed2f3a76bdb5a29f22899035f
Alexander Volkov <agvolkov4@yandex.ru>
2019-05-04 16:25:50 +0300

    added getToken function

--- src/simple.cpp
+++ src/simple.cpp
@@ -1,8 +1,12 @@
+void func(float f1, int f2) {
+    return 0;
+}
+
 int main()
 {
      /* Main Function Code */
     int a, b[5], c;
-    char d;
+    char d[1][4][100];
     a = b + 5;
      return 0;
 }
